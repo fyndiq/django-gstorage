b7d8daf8377fff86662bb8721b1008667e8a4767
Pradip Caulagi <caulagi@gmail.com>
2016-10-24 16:52:27 +0200

    Add a one-liner about module functionality and tidy double quotes

--- gstorage/__init__.py
+++ gstorage/__init__.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 """
+    django-gstorage
+    ~~~~~~~~~~~~~~~
+
+    A plug-and-play replacement for FileSystemStorage but
+    using Google storage for persistence
 """
 
-__version__ = "0.0.1.1"
-__author__ = "Fyndiq"
-__license__ = "MIT"
-__copyright__ = "Copyright 2016 Fyndiq AB"
+__version__ = '0.0.1.1'
+__author__ = 'Fyndiq'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Fyndiq AB'
