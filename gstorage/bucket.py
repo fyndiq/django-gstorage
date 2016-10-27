621d5fb9ddc38cc662edd37484e910056de34858
Pradip Caulagi <caulagi@gmail.com>
2016-10-27 15:27:33 +0200

    Method to get or create a bucket

--- gstorage/bucket.py
+++ gstorage/bucket.py
@@ -4,21 +4,36 @@ A thin wrapper around gcloud.bucket with convenience
 methods to interact with buckets
 
     >>> from gstorage.bucket import Bucket
-    >>> bucket = Bucket()
+    >>> bucket = Bucket.get_or_create('pcaulagi')
     >>> bucket.sync_dir('/share/images')
 """
 
 from gcloud.storage.bucket import Bucket as BaseBucket
 from gcloud.storage.client import Client
+from oauth2client.client import GoogleCredentials
 
 from .utils import get_config
 
 
 class Bucket(BaseBucket):
 
-    def __init__(self, bucket=None, project=None):
-        credentials = get_config('GOOGLE_APPLICATION_CREDENTIALS')
-        if not project:
-            project = get_config('GCLOUD_PROJECT_NAME')
-        client = Client(credentials=credentials, project=project)
-        return super(Bucket, self).__init__(client, bucket)
+    @classmethod
+    def get_or_create(cls, bucket_name, client=None):
+        """
+        If the bucket exists with this name, get it. Else, create it
+
+        Args:
+            bucket_name: (string) name of the bucket
+        Returns:
+            an instance of bucket
+        Raises:
+            400: Bad request (the name is not valid)
+            403: Forbidden (You don't have permissions for creating bucket)
+        """
+        if not client:
+            credentials = GoogleCredentials.get_application_default()
+            client = Client(credentials=credentials)
+        bucket = cls(client, name=bucket_name)
+        if not bucket.exists():
+            bucket.create()
+        return bucket
--- tests/test_bucket.py
+++ tests/test_bucket.py
@@ -2,7 +2,7 @@
 """
 Tests for gstorage.bucket
 """
-from mock import patch
+from mock import patch, MagicMock
 from os import environ
 from unittest import TestCase
 
@@ -11,25 +11,31 @@ from gstorage.bucket import Bucket
 
 class TestBucket(TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         try:
             del environ['GOOGLE_APPLICATION_CREDENTIALS']
         except KeyError:
             pass
 
     @patch('gstorage.bucket.Client')
-    def test_default_constructor(self, mock_client):
-        bucket = Bucket()
-        assert bucket is not None
-        mock_client.assert_called_once_with(credentials=None, project=None)
+    def test_call_to_client(self, mock_client):
+        bucket = Bucket.get_or_create('test')
+        assert mock_client.called
 
     @patch('gstorage.bucket.Client')
-    def test_project(self, mock_client):
-        Bucket(project='test')
-        mock_client.assert_called_once_with(credentials=None, project='test')
+    def test_no_call_to_client(self, mock_client):
+        bucket = Bucket.get_or_create('test', client=mock_client)
+        assert not mock_client.called
 
-    @patch('gstorage.bucket.Client')
-    def test_credentials(self, mock_client):
-        environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/tmp/test'
-        Bucket()
-        mock_client.assert_called_once_with(credentials='/tmp/test', project=None)
+    @patch('gstorage.bucket.Bucket.create')
+    def test_bucket_exists(self, mock_create):
+        Bucket.exists = MagicMock(return_value=True)
+        bucket = Bucket.get_or_create('test', client='test')
+        assert not mock_create.called
+
+    @patch('gstorage.bucket.Bucket.create')
+    def test_bucket_doesnot_exist(self, mock_create):
+        Bucket.exists = MagicMock(return_value=False)
+        bucket = Bucket.get_or_create('test', client='test')
+        assert mock_create.called
