355dd2e3b7b2c8525f831a140da5f3a449867d37
Pradip Caulagi <caulagi@gmail.com>
2016-11-02 13:14:23 +0100

    Rename blog -> settings

--- examples/blog/settings/base.py
+++ examples/blog/settings/base.py
@@ -50,7 +50,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'blog.urls'
+ROOT_URLCONF = 'settings.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'blog.wsgi.application'
+WSGI_APPLICATION = 'settings.wsgi.application'
 
 
 # Database
