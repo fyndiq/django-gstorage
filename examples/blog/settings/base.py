4abdd78a696a6b51d9b1ee5e1ada9416103a37bd
Pradip Caulagi <caulagi@gmail.com>
2016-11-02 14:08:00 +0100

    Fix reading environment

--- examples/blog/settings/base.py
+++ examples/blog/settings/base.py
@@ -16,7 +16,9 @@ import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 env = environ.Env()
+env.read_env('.env')
 
 
 # Quick-start development settings - unsuitable for production
